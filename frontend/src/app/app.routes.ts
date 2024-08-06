4c9de09fc6dac10787c4789bc486865d9fb1bbbd
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2024-08-06 14:37:35 +0100

    frontend: add home component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './feature/landing-page/landing-page.component';
+import { HomeComponent } from './feature/home/home.component';
 
 export const routes: Routes = [
     {path:'',component: LandingPageComponent},
+    {path:'home',component: HomeComponent},
 ];
